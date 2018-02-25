73d4ca3da6ddb80b37ffe87b38defa94073b2235
juwanhong <jwhong0503@gmail.com>
2018-02-24 21:32:39 -0500

    commit

--- src/tls.java
+++ src/tls.java
@@ -1,4 +1,20 @@
 
 public class tls {
 
+	public static void main(String[] args) {
+		String mode = args[0];
+		
+		switch(mode) {
+		case "-s":
+			server.Server(args);
+			break;
+			
+		case "-c":
+			client.Client(args);
+			break;
+			
+		default:
+			break;
+		}
+	}
 }
